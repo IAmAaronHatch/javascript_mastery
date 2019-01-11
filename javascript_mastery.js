9ab700ce875135f8b42bf13b9d727694cdafc3ce
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-01-11 08:19:05 -0800

    added another practice problem about summation

--- javascript_mastery.js
+++ javascript_mastery.js
@@ -11,3 +11,20 @@ function noSpace(x) {
     return x.replace(/\s/g, '');
 }
 console.log(noSpace('Example Tests For this function'))
+
+
+// Reverse List Order 
+    // take in a list(array) and return it in reverse order
+
+function reverseList(list) {
+    return list.reverse()
+}
+
+console.log(reverseList([1,2,3,4]))
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+var summation = function (n) {
+    return n * (n + 1)/2
+}
+console.log(summation(8))
